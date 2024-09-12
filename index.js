f9c70363e306da59ab23163b8ce8e336fbe47e81
beatriz <carolinebeatrizads@gmail.com>
2024-09-12 18:30:50 -0300

    resolve problema marcadas/desmarcadas

--- index.js
+++ index.js
@@ -33,14 +33,14 @@ const listarMetas = async () => {
         instructions: false
     })
 
-    if(respostas.length == 0){
-        console.log("Nenhuma meta foi selecionada.")
-        return
-    }
-
     metas.forEach((m) => {
         m.checked = false
     })
+
+    if(respostas.length == 0){
+        console.log("Nenhuma meta foi selecionada.")
+        return
+    }  
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
